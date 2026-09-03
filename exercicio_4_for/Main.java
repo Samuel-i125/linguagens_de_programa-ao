150fe14d9c9bf7b03346031be584b48060608915
Samuel-i125 <samueldonas11@gmail.com>
2026-09-03 01:28:46 -0300

    exercicio

---
+++ exercicio_4_for/Main.java
@@ -0,0 +1,23 @@
+package exercicio_4_for;
+
+public class Main {
+    public static void main (String[] args) {
+        for(int i = 0; i < 5;i++) {
+            System.out.println(i);
+
+        }
+      
+        for(int i=100;i >0; i--) {
+            System.out.println(i);
+        }
+
+        for (int x = 0; x < 2; x++) {
+            for(int y = 0; y < 2; y++) {
+                System.out.print("#");
+            }
+            System.out.println ("");
+        }
+
+
+    }
+}
---
+++ exercicio_5_foreach/Main.java
@@ -0,0 +1,44 @@
+package exercicio_5_foreach;
+
+public class Main {
+     public static void main(String[] args) { 
+     /*    String[] alunos = {"Miranata", "Savalo Horse", "Aeronauta"};
+
+        alunos [0] = "Mariazinha";
+        System.out.println("Qtd de Alunos " + alunos.length);
+        
+        for(String estudante : alunos ) {
+            System.out.println(estudante);
+        }
+    } */ 
+/* 
+    String [] produto = {"Shampoo", "Sabonete", "Condicionador", "Pasta de dente", "Escova"};
+    for (String Higiene : produto) {
+    System.out.println (Higiene);
+    } */
+
+    /* String [] produto = {"Shampoo", "Sabonete", "Condicionador", "Pasta de dente", "Escova"};
+    for (int i=0; i < produto.length; i++) {
+        System.out.println (produto [i]);
+    } */
+
+    int[] numeros = {-1, 0, 3, 4, 5};
+    
+    for (int algarismo : numeros)
+
+    if (algarismo > 0) {
+        System.out.println ("Positivo " + algarismo);
+        if (algarismo == 0 ) 
+        System.out.println ("o numero " + algarismo);
+        if (algarismo < 0)
+        System.out.println ( "Negativo " + algarismo);
+        }
+
+
+    }
+
+
+
+    
+}
+
